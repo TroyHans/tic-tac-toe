ebc656605650a078100696d6cd68cd5ee11e3dbf
Troy Hans <xorak420@gmail.com>
2023-02-26 19:03:25 -0500

    removed the globals

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,10 +5,10 @@ from random import randint
 from os import system
 from time import sleep
 
-BOARD = ['']+[str(x) for x in range(1, 10)]
-PLAYER = 'X'
-COMPUTER = 'O'
-CURRENT_PLAYER = PLAYER
+board = ['']+[str(x) for x in range(1, 10)]
+player = 'X'
+computer = 'O'
+current_player = player
 
 def game():
     '''running the game'''
@@ -22,29 +22,26 @@ def game():
 
 def display_board():
     '''clears the screen and displays the game board'''
-    global BOARD
     system('clear')
     print('\t TIC TAC TOE')
     print('\t-------------')
-    print(f'\t| {BOARD[7]} | {BOARD[8]} | {BOARD[9]} |')
+    print(f'\t| {board[7]} | {board[8]} | {board[9]} |')
     print('\t-------------')
-    print(f'\t| {BOARD[4]} | {BOARD[5]} | {BOARD[6]} |')
+    print(f'\t| {board[4]} | {board[5]} | {board[6]} |')
     print('\t-------------')
-    print(f'\t| {BOARD[1]} | {BOARD[2]} | {BOARD[3]} |')
+    print(f'\t| {board[1]} | {board[2]} | {board[3]} |')
     print('\t-------------')
     print('\n')
 
 def player_select():
     '''The player inputs a square selection'''
-    global BOARD
-    global CURRENT_PLAYER
-    CURRENT_PLAYER = PLAYER
+    current_player = player
     while True:
         try:
             display_board()
-            user_input = int(input("\t   Player\nPlease choose a square 1 - 9: "))
-            if user_input in range(1, 10) and BOARD[user_input].isdigit():
-                BOARD[user_input] = PLAYER
+            user_input = int(input("\t   player\nPlease choose a square 1 - 9: "))
+            if user_input in range(1, 10) and board[user_input].isdigit():
+                board[user_input] = player
                 break
             print('You must select an unplayed square')
             sleep(2)
@@ -53,16 +50,14 @@ def player_select():
 
 def computer_select():
     '''The computer dumb selects a random available square'''
-    global BOARD
-    global CURRENT_PLAYER
-    CURRENT_PLAYER = COMPUTER
+    current_player = computer
     display_board()
-    print('   The Computer is playing')
+    print('   The computer is playing')
     sleep(randint(2, 5))
     while True:
         comp_choice = randint(1, 9)
-        if BOARD[comp_choice].isdigit():
-            BOARD[comp_choice] = COMPUTER
+        if board[comp_choice].isdigit():
+            board[comp_choice] = computer
             display_board()
             print(f'   I have chosen square #{comp_choice}')
             sleep(2)
@@ -70,20 +65,18 @@ def computer_select():
 
 def check_for_win_or_tie():
     '''Checks for a win or tie'''
-    global BOARD
-    global CURRENT_PLAYER
     display_board()
-    if (BOARD[1] == BOARD[2] == BOARD[3] or
-        BOARD[4] == BOARD[5] == BOARD[6] or
-        BOARD[7] == BOARD[8] == BOARD[9] or
-        BOARD[1] == BOARD[4] == BOARD[7] or
-        BOARD[2] == BOARD[5] == BOARD[8] or
-        BOARD[3] == BOARD[6] == BOARD[9] or
-        BOARD[1] == BOARD[5] == BOARD[9] or
-        BOARD[3] == BOARD[5] == BOARD[7]):
-        print(f'\t   {CURRENT_PLAYER} WINS!')
+    if (board[1] == board[2] == board[3] or
+        board[4] == board[5] == board[6] or
+        board[7] == board[8] == board[9] or
+        board[1] == board[4] == board[7] or
+        board[2] == board[5] == board[8] or
+        board[3] == board[6] == board[9] or
+        board[1] == board[5] == board[9] or
+        board[3] == board[5] == board[7]):
+        print(f'\t   {current_player} WINS!')
         return True
-    if ''.join(BOARD[1:]).isalpha():
+    if ''.join(board[1:]).isalpha():
         print('\tThe game is a Tie')
         return True
     
